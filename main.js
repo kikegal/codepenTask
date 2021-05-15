e10ae4ea181bd6707c06b3a3051f6975c27cb154
Enrique Garcia Lopez <53075322+kikegal@users.noreply.github.com>
2021-05-15 01:29:38 -0300

    Survey form - validation form - freecodecamp - codepen
    Survey form - validation form - freecodecamp - codepen

---
+++ main.js
@@ -0,0 +1,21 @@
+function verificar(){
+
+    let nome = document.getElementById('txtnome')
+    let birth = document.getElementById('birth')
+    let origem = document.getElementById('origem')
+    let email = document.getElementById('email')
+    let opcoes = document.getElementById('dropdown')
+    let meioT= document.getElementById('transp')
+    let info = document.getElementsByName('info')
+    let avaliacao = document.getElementById('numbervalue')
+    let opinion = document.getElementById('txtml')
+
+    if (txtnome.value.lenght == 0){
+        window.alert('Fill the Name')
+    }else{
+        alert('ok')
+    }
+
+
+}
+
